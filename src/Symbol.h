1acda8550c60a66640c87c742306c2c6f39106aa
MartinRemi <mrmi11@yahoo.fr>
2015-03-05 15:00:19 +0100

    Fixed symbols

--- src/Symbol.h
+++ src/Symbol.h
@@ -6,6 +6,24 @@
 #ifndef _SYMBOL_H
 #define _SYMBOL_H
 
-enum Symbol {  };
+enum Symbol {
+	Pprim,
+	P,
+	Pd,
+	D,
+	Lval,
+	Lconst,
+	Pi,
+	I,
+	E,
+	pv,
+	aff,
+	vg,
+	n,
+	w,
+	r,
+	v,
+	c
+};
 
 #endif //_SYMBOL_H
